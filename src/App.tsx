ff6d47bf61e9de58ef2280e5087b7fb2ad2a685f
timpriadko <timpriadko@gmail.com>
2024-01-22 23:04:49 +0200

    add TW, scss, Nav data and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
 import Navigation from './components/Navigation';
 
-import './style.css';
+import './style.scss';
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
+      <Navigation />
       <h1>Hello {name}!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <Navigation />
     </div>
   );
 };
